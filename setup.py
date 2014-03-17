a84b2313487bce5dd2787e665eb6b4a1330933be
Rafael Canovas <rafaelcanovas@me.com>
2014-03-17 18:55:31 -0300

    Bump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except (ImportError, OSError, IOError):
 
 setup(
 	name='django-alacarte',
-	version='0.1.1',
+	version='0.1.2',
 	description='A minimalist Django menu app.',
 	long_description=readme,
 	author='Rafael Canovas',
