d2004d87396c4dfe0c73258dba8d15d11e3c9cca
Rafael Canovas <rafaelcanovas@me.com>
2014-03-17 18:55:05 -0300

    Added active menu detection

--- alacarte/menus.py
+++ alacarte/menus.py
@@ -2,20 +2,39 @@ from django.core.urlresolvers import reverse, NoReverseMatch
 
 
 class Menu:
-	def shown(self):
+	@classmethod
+	def shown(cls):
 		"""
 		All menus are shown by default.
 		Override this method to implement custom behavior.
 		"""
 		return True
 
-	def url(self):
+	@classmethod
+	def url(cls):
 		"""
 		Try to reverse `url_name`, fallback to '#' if not possible.
 		"""
 		try:
-			return reverse(self.url_name)
+			return reverse(cls.url_name)
 		except AttributeError:
 			return '#'
 		except NoReverseMatch:
 			raise
+
+	@classmethod
+	def is_active(cls):
+		"""
+		A menu is active either if its `url_name` is the current or if
+		any of its `submenus` are active.
+		"""
+		url = sub_urls = False
+		if hasattr(cls, 'url_name'):
+			url = reverse(cls.url_name) == cls.context['request'].path
+		if hasattr(cls, 'submenus'):
+			sub_urls = any([s.is_active() for s in cls.submenus])
+
+		if url or sub_urls:
+			print('%s is active' % cls.label)
+
+		return url or sub_urls
