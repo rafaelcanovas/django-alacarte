02d9452eaa88e533e7da89741962a09cc802f607
Rafael Canovas <rafaelcanovas@me.com>
2014-10-31 23:11:51 -0200

    Rendering test and 1.7 compatibility on tests

--- alacarte/tests.py
+++ alacarte/tests.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python
 
+import os
 import unittest
 import alacarte
 
+BASE_DIR = os.path.realpath(os.path.dirname(__file__))
+
 from django.conf import settings
-settings.configure()
+if not settings.configured:
+	settings.configure(
+		TEMPLATE_DIRS=(os.path.join(BASE_DIR, 'templates'),)
+	)
+
+import django
+if hasattr(django, 'setup'):
+	django.setup()
+
+from django.template.loader import render_to_string
+from django.template import Context
 
 
 class AlacartTestCase(unittest.TestCase):
@@ -12,9 +25,13 @@ class AlacartTestCase(unittest.TestCase):
 		"""
 		Create sample menus for testing.
 		"""
+		class TestSubMenu(alacarte.Menu):
+			label = 'test sub menu'
+
 		class TestMenu(alacarte.Menu):
 			group = 'test'
 			label = 'test menu'
+			submenus = (TestSubMenu(),)
 
 		class TestMenu2(alacarte.Menu):
 			group = 'test'
@@ -40,6 +57,12 @@ class AlacartTestCase(unittest.TestCase):
 		alacarte.clear_registry()
 		alacarte.register(self.test_menu, self.test_menu_2)
 		self.assertEqual(len(alacarte.get_menus('test')), 2)
+
+	def test_render(self):
+		c = Context({'menus': alacarte.get_menus('test')})
+		t = render_to_string('alacarte/menu.html', c)
+		print(t)
+		self.assertNotEqual(len(t), 0)
 
 
 if __name__ == '__main__':
